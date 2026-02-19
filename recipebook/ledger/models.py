f9a75bbdd06dddebcd8c497099d81e4ac8453da3
Krystal <krystal.limtiongsoon@student.ateneo.edu>
2026-02-19 20:37:04 +0800

    minor fix

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -6,7 +6,7 @@ class Ingredient(models.Model):
     def __str__(self):
         return f"{self.name}"
     def get_absolute_url(self):
-        return reverse('ingredients', args=[str(self.id)])
+        return reverse('ledger:ingredient_detail', args=[str(self.id)])
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
