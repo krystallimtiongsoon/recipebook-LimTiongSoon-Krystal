7444d49c2a8506614ee5953fb32b5b0c027c46e2
Krystal <krystal.limtiongsoon@student.ateneo.edu>
2026-02-19 09:40:43 +0800

    created functions in models
    also condensed recipe htmls

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,13 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('ingredients:recipe_detail', args=[str(self.id)])
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('ledger:recipes_list', args=[str(self.id)])
 
 class RecipeIngredient(models.Model):
-    quantity = models.CharField(max_length=5)
+    quantity = models.CharField(max_length=100)
     ingredient = models.ForeignKey(
         Ingredient,
         on_delete=models.CASCADE,
--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import recipes_list , recipe_1, recipe_2
+from .views import RecipesListView, RecipeDetailView
 
 urlpatterns = [
-    path('recipes/list', recipes_list,name="recipes_list"),
-    path('recipe/1', recipe_1, name="recipe_1"),
-    path('recipe/2', recipe_2, name="recipe_2"),
+    path('recipes/list', RecipesListView,name="recipes_list"),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name="recipe_detail"),
 ]
 
 app_name='ledger'
