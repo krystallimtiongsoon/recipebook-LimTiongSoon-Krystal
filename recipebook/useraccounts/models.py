8c24d580ecea15cb7f7024bf249c639c3f1f215a
Krystal <krystal.limtiongsoon@student.ateneo.edu>
2026-03-02 09:37:40 +0800

    debugging

--- recipebook/useraccounts/models.py
+++ recipebook/useraccounts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Profile (models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     bio = models.TextField(blank=True)
 
-
+    def __str__(self):
+        return f"{self.name}"
