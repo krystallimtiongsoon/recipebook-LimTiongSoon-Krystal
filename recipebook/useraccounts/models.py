a90959412b4e5e20abbe78d53cdb59096b0fc27f
Krystal <krystal.limtiongsoon@student.ateneo.edu>
2026-03-02 19:22:26 +0800

    fixed bio in profile model

--- recipebook/useraccounts/models.py
+++ recipebook/useraccounts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
 
 class Profile (models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    bio = models.TextField(blank=True)
+    bio = models.TextField(validators=[MinLengthValidator(256)])
 
     def __str__(self):
         return f"{self.name}"
