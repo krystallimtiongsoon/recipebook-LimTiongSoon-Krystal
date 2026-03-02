8160c4157afd366007ab0800e582291d4075701b
Krystal <krystal.limtiongsoon@student.ateneo.edu>
2026-03-02 08:44:04 +0800

    modified recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.urls import reverse
+from .models import Profile
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return f"{self.name}"
     def get_absolute_url(self):
@@ -10,6 +12,14 @@ class Ingredient(models.Model):
     
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return f"{self.name}"
     def get_absolute_url(self):
