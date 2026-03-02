083c68b6dc15c1bd45c041198cc7dc308b42a5bf
Krystal <krystal.limtiongsoon@student.ateneo.edu>
2026-03-02 09:10:03 +0800

    added login, logout, and password reset flows

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,8 +1,9 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = "recipe_details.html"
 
--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = '/recipes/list'
+LOGOUT_REDIRECT_URL = '/accounts/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
