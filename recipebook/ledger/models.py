a8a42c03e80b7125f5aac15c9cd3eb965fbd5bbb
Krystal <krystal.limtiongsoon@student.ateneo.edu>
2026-03-12 08:16:38 +0800

    created RecipeImage model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -42,3 +42,15 @@ class RecipeIngredient(models.Model):
         on_delete=models.CASCADE,
         related_name='ingredients'
     )
+
+
+class RecipeImage(models.Model):
+    image = models.ImageField(
+        upload_to='images/'
+    )
+    description = models.CharField(max_length=255)
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='image'
+    )
